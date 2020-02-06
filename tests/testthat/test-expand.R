cc01c3849c298d8a17c6f453c59e860ed4dbc8c4
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-06 07:45:35 +0100

    Add test

--- tests/testthat/test-expand.R
+++ tests/testthat/test-expand.R
@@ -12,6 +12,14 @@ test_that("simple expand example works", {
   }))
 })
 
+test_that("missing arguments are supported", {
+  fun <- function() {
+    iris[1, ]
+  }
+
+  expect_identical(expand_function(fun), fun)
+})
+
 test_that("expanding a macro retains attributes", {
   add <- defmacro(function() {
     1
